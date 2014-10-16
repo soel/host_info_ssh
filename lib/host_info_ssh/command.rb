ab341f66495a760fb15da54a0a25f477205401e9
kanbara <kanbara@bbtower.co.jp>
2014-10-16 20:06:57 +0900

    fixes default options

--- lib/host_info_ssh/command.rb
+++ lib/host_info_ssh/command.rb
@@ -3,23 +3,38 @@ module HostInfoSsh
 
   class Command
         
-    def self.run(argv)
-      options = Options.parse!(@argv)
-      
-      #p options
-      #p options[:n]
-      #p options[:u]
-      command_list = FileOpen.read_file('lib/host_info_ssh/command_list.txt')
-      host_info, err_info = Ssh.connect_ssh(options[:hostname], options[:username], command_list)
-      p host_info
-      #p err_info
-      #output = Outputformat.markdown(argv[0], host_info, err_info)
-      #puts output
-      #Fileout.output_file("test.md", output)
-    end
-    
     def initialize(argv)
       @argv = argv
+    end
+    
+    def self.run(argv)
+      new(argv).execute
+    end
+    
+    def execute
+      
+      options = Options.parse!(@argv)
+      #p @command_list
+      if options[:commandfile]
+        command_list_file = options[:commandfile]
+      else
+        command_list_file = 'lib/host_info_ssh/command_list.txt'
+      end
+      
+      if options[:outputfile]
+        output_file = options[:outputfile]
+      else
+        output_file = "#{options[:nodename]}.md"
+      end
+      
+      command_list = FileOpen.read_file(command_list_file)
+      host_info, err_info = Ssh.connect_ssh(options[:nodename], options[:username], command_list)
+      #p host_info
+      #p err_info
+      output = Outputformat.markdown(options[:nodename], host_info, err_info)
+      #puts output
+      p output_file
+      Fileout.output_file(output_file, output)
     end
   end
 end
--- lib/host_info_ssh/command/options.rb
+++ lib/host_info_ssh/command/options.rb
@@ -7,22 +7,31 @@ module HostInfoSsh
     module Options
       
       def self.parse!(argv)
-       options = {}
-       #command_parser = OptionParser.new do |opt|
-       OptionParser.new do |opt|
-         opt.on_head('-v', '--version', 'show program version') do |v|
-           opt.version = HostInfoSsh::VERSION
-           puts opt.ver
-           exit
-         end
-         #opt.on('-h', '--help', 'help message'){puts options; exit}
-         opt.on('-n NODENAME', '--nodename', 'hostname or ipaddress') {|v| options[:hostname] = v}
-         opt.on('-u USERNAME', '--username', 'username') {|v| options[:username] = v}
-         
-         opt.parse!
-       end
-       
-       return options
+        options = {}
+
+        OptionParser.new do |opt|
+          opt.on_head('-v', '--version', 'show program version') do |v|
+            opt.version = HostInfoSsh::VERSION
+            puts opt.ver
+            exit
+          end
+          opt.on('-n NODENAME', '--nodename', 'hostname or ipaddress') {|v| options[:nodename] = v}
+          opt.on('-u USERNAME', '--username', 'username') {|v| options[:username] = v}
+          opt.on('-c COMMANDFILENAME', '--commandfile', 'commandfilename') {|v| options[:commandfile] = v}
+          opt.on('-o OUTPUTFILENAME', '--outputfile', 'outputfilename') {|v| options[:outputfile] = v}
+          begin
+            opt.parse!
+          rescue => e #定義されていないオプションが指定された時の処理
+            puts "ERROR: #{e}.\n #{opt}"
+            exit
+          end
+          unless options[:nodename] && options[:username] #必須のオプションがない場合の処理
+            puts "#{opt}"
+            exit
+          end
+        end
+        
+        return options
       end
     end
   end
