2b7bb9c34305befb084beb39d8bbf385499f4427
kanbara <kanbara@bbtower.co.jp>
2014-10-17 12:09:47 +0900

    Fixes #5 ssh timeout error

--- lib/host_info_ssh/ssh.rb
+++ lib/host_info_ssh/ssh.rb
@@ -1,33 +1,51 @@
 # coding: utf-8
 
 require "net/ssh"
+require "timeout"
 
 module HostInfoSsh
   
   module Ssh
 
     def self.connect_ssh(host, user, command_list)
-      stdout_list = []
-      stderr_list = ["error:"]
-      
-      Net::SSH.start(host, user) do |ssh|
-        command_list.each do |command|
-          ssh.exec!(command) do |channel, stream, data|
-            stdout = "command: #{command}"
-            stderr = ""
-            stdout << data if stream == :stdout
-            stderr << data if stream == :stderr
-            if stderr == "" 
-                stdout_list.push(stdout)
-            else
-                stdout_list.push("")
-                stderr_list.push(stderr)
+      begin
+        stdout_list = []
+        stderr_list = ["error:"]
+        
+        Timeout::timeout(5) do
+          begin
+            Net::SSH.start(host, user) do |ssh|
+              command_list.each do |command|
+                ssh.exec!(command) do |channel, stream, data|
+                  stdout = "command: #{command}"
+                  stderr = ""
+                  stdout << data if stream == :stdout
+                  stderr << data if stream == :stderr
+                  if stderr == "" 
+                    stdout_list.push(stdout)
+                  else
+                    stdout_list.push("")
+                    stderr_list.push(stderr)
+                  end
+                end
+              end
             end
+          
+          rescue Net::SSH::HostKeyMismatch => e
+            puts e
+            exit
+          rescue StandardError => e
+            puts e
+            exit
           end
         end
+        
+        return stdout_list, stderr_list
+      
+      rescue Timeout::Error
+        puts "Timed out trying to get a connection"
+        exit
       end
- 
-      return stdout_list, stderr_list
     end
   end
 end
